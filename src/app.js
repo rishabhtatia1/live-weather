aaac91dc0e056eec24cd082359a0ce3c4add2415
rishabhtatia <rishabh.tatia@codeastu.com>
2021-05-14 21:59:35 +0530

    Added functionality for fetch the weather data from weatherpage

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
 const app = express();
+
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -12,7 +16,7 @@ hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
-  res.render("index", { title: "Weather", name: "Rishabh Tatia" });
+  res.render("index", { title: "Weather", name: "Rishabh Tatia", helperText: "Use this site to get your weather!" });
 });
 
 app.get("/help", (req, res) => {
@@ -24,10 +28,24 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
-  res.send({ forecast: "It is snowing", location: "Boston" });
+  if (!req.query.address) {
+    return res.send({ error: "You must provide an address." });
+  }
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, foreCastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({ forecast: foreCastData, location, address: req.query.address });
+    });
+  });
+  // res.send({ forecast: "It is snowing", location: "Boston", address: req.query.address });
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.render("404", { title: "404", name: "Rishabh Tatia", message: "Page not found" });
 });
 
